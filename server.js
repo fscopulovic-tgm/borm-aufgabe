792cd31457815bbd025b79a84d2b583708e05ade
fscopulovic-tgm <fscopulovic@student.tgm.ac.at>
2017-11-05 22:21:44 +0100

    Sign Up works now, login needs to be implemented

--- server.js
+++ server.js
@@ -12,19 +12,28 @@ MongoClient.connect('mongodb://fscopulovic:BormAufgabe1@ds249025.mlab.com:49025/
   if (err) return console.log(err)
   db = database
   app.listen(3000, () => {
-    console.log('listening on 3000')
+    console.log('server started at localhost:3000')
   })
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/views/login.html')
+})
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/views/signup.html')
+})
+
+app.post('/login', (req, res) => {
+	console.log('login')
+	res.redirect('/')
 })
 
 app.post('/createUser', (req, res) => {
   db.collection('users').save(req.body, (err, result) => {
     if (err) return console.log(err)
 
-    console.log('saved to database')
+    console.log('Saved to the database')
     res.redirect('/')
   })
 })
