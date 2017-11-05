b173200f05e8431f0f029d91d60f571479f0b812
fscopulovic-tgm <fscopulovic@student.tgm.ac.at>
2017-11-05 21:28:24 +0100

    Creating a user in the db works now

--- server.js
+++ server.js
@@ -2,16 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient
+
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+var db
+
+MongoClient.connect('mongodb://fscopulovic:BormAufgabe1@ds249025.mlab.com:49025/scopulovic-borm', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
 })
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.post('/createTest', (req, res) => {
-  console.log(req.body)
+app.post('/createUser', (req, res) => {
+  db.collection('users').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
